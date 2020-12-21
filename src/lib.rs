6c8e34a5590944ce2044a05be866e8ca00e1d824
Nakano Masaki <namachan10777@gmail.com>
2020-12-22 06:01:13 +0900

    construct AST

--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,7 @@ pub enum Stmt {
 pub enum Flat {
     // u8で指定されたレジスタ中の文字列を出力
     Print(u8),
-    // u16で指定された文字列領域の文字列リテラルをregに格納
-    SConst(u16, u8),
+    // u16で指定された文字列領域の文字列リテラルをregに格納 SConst(u16, u8),
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -46,6 +45,7 @@ pub fn entry(src_path: &str) -> Result<(), Error> {
     let src = fs::read_to_string(src_path).map_err(|_| Error::IOError)?;
     let tokens = lexer::lex(&src)?;
     let ast = parser::ll1(tokens)?;
+    println!("{:?}", ast);
     let flat = ir::compile(ast);
     let mut vm = vm::Vm::compile(flat);
     vm.execute()
