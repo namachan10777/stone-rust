7b910c0438c0421af8e287bb633d913d0c818243
Nakano Masaki <namachan10777@gmail.com>
2020-12-25 07:06:35 +0900

    lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,97 +3,134 @@ use crate::Error;
 
 const PRINT: &str = "print";
 
+fn match_str(src: &str, compare: &str) -> Option<usize> {
+    if compare.len() > src.len() {
+        return None;
+    }
+    if &src[0..compare.len()] == compare {
+        return Some(compare.len());
+    }
+    else { return None;
+    }
+}
+
+// TODO 実装サボっており整数のみしか扱えません
+fn match_num(src: &str) -> Option<usize> {
+    if src.is_empty() {
+        return None;
+    }
+    let mut cnt = 0;
+    while cnt < src.len() {
+        // めちゃくちゃバカっぽいコードに見えますが、Rustの仕様だと多分これが一番速い
+        match &src[cnt..cnt+1] {
+            "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" => {},
+            _ => {
+                break;
+            }
+        }
+        cnt += 1;
+    }
+    if cnt != 0 {
+        return Some(cnt);
+    }
+    else {
+        return None;
+    }
+}
+
+fn match_white(src: &str) -> Option<usize> {
+    if src.is_empty() {
+        return None;
+    }
+    let mut cnt = 0;
+    while cnt < src.len() {
+        // めちゃくちゃバカっぽいコードに見えますが、Rustの仕様だと多分これが一番速い
+        match &src[cnt..cnt+1] {
+            " " | "\t" | "\r" => {},
+            _ => {
+                break;
+            }
+        }
+        cnt += 1;
+    }
+    if cnt != 0 {
+        return Some(cnt);
+    }
+    else {
+        return None;
+    }
+}
+
 pub fn lex(src: &str) -> Result<Vec<Token>, Error> {
     let mut tokens = Vec::new();
-    let mut beg = 0;
-    loop {
-        if beg == src.len() {
-            tokens.push(Token::Eof);
-            break;
-        } else if (src.len() - beg) >= PRINT.len() && src[beg..beg + PRINT.len()].eq(PRINT) {
-            beg += PRINT.len();
-            tokens.push(Token::Print);
-        } else if src[beg..beg + 1].eq(" ")
-            || src[beg..beg + 1].eq("\t")
-            || src[beg..beg + 1].eq("\r")
-        {
-            // skip
-            beg += 1;
-        } else if src[beg..beg + 1].eq("\n") {
-            tokens.push(Token::Eol);
-            beg += 1;
-        } else if (src.len() - beg) >= 2 && src[beg..beg + 1].eq("\"") {
-            let mut s = String::new();
-            beg += 1;
-            let mut end = beg;
-            loop {
-                if (src.len() - end) >= 2 && src[end..end + 1].eq("\\") {
-                    s.push_str(&src[beg..end]);
-                    match &src[end + 1..end + 2] {
-                        "\"" | "\\" => s.push_str(&src[end + 1..end + 2]),
-                        "n" => s.push('\n'),
-                        _ => return Err(Error::SyntaxError),
-                    }
-                    end += 2;
-                    beg = end;
-                } else if src.len() - end >= 1 && src[end..end + 1].eq("\n") {
-                    return Err(Error::SyntaxError);
-                } else if src.len() - end >= 1 && src[end..end + 1].eq("\"") {
-                    s.push_str(&src[beg..end]);
-                    tokens.push(Token::StrLit(s));
-                    end += 1;
-                    beg = end;
-                    break;
-                } else if (src.len() - end) == 0 {
-                    return Err(Error::SyntaxError);
-                } else {
-                    end += 1;
-                }
-            }
-        } else {
+    let mut begin = 0;
+    while begin < src.len() {
+        let remain = &src[begin..src.len()];
+        if let Some(step) = match_white(remain) {
+            begin += step;
+        }
+        else if let Some(step) = match_num(remain) {
+            tokens.push(Token::Num(src[begin..begin+step].parse().unwrap()));
+            begin += step;
+        }
+        else if let Some(step) = match_str(remain, "(") {
+            tokens.push(Token::LP);
+            begin += step;
+        }
+        else if let Some(step) = match_str(remain, ")") {
+            tokens.push(Token::RP);
+            begin += step;
+        }
+        else if let Some(step) = match_str(remain, "+") {
+            tokens.push(Token::Add);
+            begin += step;
+        }
+        else if let Some(step) = match_str(remain, "-") {
+            tokens.push(Token::Sub);
+            begin += step;
+        }
+        else if let Some(step) = match_str(remain, ";") {
+            tokens.push(Token::Semicolon);
+            begin += step;
+        }
+        else if let Some(step) = match_str(remain, "{") {
+            tokens.push(Token::LB);
+            begin += step;
+        }
+        else if let Some(step) = match_str(remain, "}") {
+            tokens.push(Token::RB);
+            begin += step;
+        }
+        else if let Some(step) = match_str(remain, "if") {
+            tokens.push(Token::If);
+            begin += step;
+        }
+        else if let Some(step) = match_str(remain, "else") {
+            tokens.push(Token::Else);
+            begin += step;
+        }
+        else if let Some(step) = match_str(remain, "while") {
+            tokens.push(Token::While);
+            begin += step;
+        }
+        else if let Some(step) = match_str(remain, "\n") {
+            tokens.push(Token::EOL);
+            begin += step;
+        }
+        else {
             return Err(Error::SyntaxError);
         }
     }
+    tokens.push(Token::EOF);
     Ok(tokens)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-
     #[test]
-    fn test_lex() {
-        assert_eq!(lex("print"), Ok(vec![Token::Print, Token::Eof]));
-        assert_eq!(
-            lex("\"hoge\""),
-            Ok(vec![Token::StrLit("hoge".to_owned()), Token::Eof])
-        );
-        assert_eq!(
-            lex("\"hoge\\\"fuga\""),
-            Ok(vec![Token::StrLit("hoge\"fuga".to_owned()), Token::Eof])
-        );
-        assert_eq!(
-            lex("\"hoge\\nfuga\""),
-            Ok(vec![Token::StrLit("hoge\nfuga".to_owned()), Token::Eof])
-        );
-        assert_eq!(
-            lex("print \"hoge\\nfuga\""),
-            Ok(vec![
-                Token::Print,
-                Token::StrLit("hoge\nfuga".to_owned()),
-                Token::Eof
-            ])
-        );
-        assert_eq!(
-            lex("print \"hoge\"\nprint \"fuga\""),
-            Ok(vec![
-                Token::Print,
-                Token::StrLit("hoge".to_owned()),
-                Token::Eol,
-                Token::Print,
-                Token::StrLit("fuga".to_owned()),
-                Token::Eof
-            ])
-        );
+    fn test() {
+        let src = "if 123 else\n  } {";
+        assert_eq!(lex(src), Ok(vec![Token::If, Token::Num(123.0), Token::Else, Token::EOL, Token::RB, Token::LB, Token::EOF]));
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,69 @@
+//! target rule
+//!  1. primary -> "(" expr ")"
+//!  2. primary -> NUMBER
+//!  3. op -> "+"
+//!  4. op -> "-"
+//!  5. expr2 -> 空
+//!  6. expr2 -> op expr
+//!  7. expr -> primary expr2
+//!  8. statement_opt -> 空
+//!  9. statement_opt -> statement
+//! 10. delim -> ";"
+//! 11. delim -> EOL
+//! 12. statement_list2 -> 空
+//! 13. statement_list2 -> delim statement_opt statement_list2
+//! 14. statement_list -> statement_opt statement_list2
+//! 15. block -> "{" statement_list "}"
+//! 16. simple -> expr
+//! 17. else_part -> 空
+//! 18. else_part -> "else" block
+//! 19. statement -> "if" expr block else_part
+//! 20. statement -> "while" expr block
+//! 21. statement -> simple
+//! 22. program -> statement_opt EOF
+
+use crate::ll1::{Terminal, NonTerminal, ll1};
+
 #[derive(Debug, PartialEq)]
 pub enum Token {
-    Print,
-    StrLit(String),
-    Eol,
-    Eof,
+    LP,
+    RP,
+    Num(f64),
+    Add,
+    Sub,
+    Semicolon,
+    EOL,
+    LB,
+    RB,
+    If,
+    Else,
+    While,
+    EOF,
+}
+
+impl Terminal for Token {
+    const N: usize = 13;
+    fn accept(&self) -> bool {
+        if let Token::Num(_) = self {
+            return true;
+        }
+        return false;
+    }
+    fn cardinal(&self) -> usize {
+        match self {
+            Token::LP => 0,
+            Token::RP => 1,
+            Token::Num(f64) =>2,
+            Token::Add =>3,
+            Token::Sub =>4,
+            Token::Semicolon =>5,
+            Token::EOL =>6,
+            Token::LB =>7,
+            Token::RB =>8,
+            Token::If =>9,
+            Token::Else =>10,
+            Token::While =>11,
+            Token::EOF=>12,
+        }
+    }
 }
